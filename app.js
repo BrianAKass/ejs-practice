606a4019ecba9771af091229d2e5ae8972bfcd24
BrianAKass <kasinator009@gmail.com>
2019-08-20 17:59:38 -0400

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date= require(__dirname + "/date.js");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -8,31 +9,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-let item = '';
-let items = [];
+
+const items = [];
+const workItems = [];
+
 app.get("/", (req,res)=>{
-    const today = new Date();
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-
-    const day = today.toLocaleDateString("en-us",options);
-
+    const day = date.getDay();
     res.render("list",{
-        today:day,
+        listTitle:day,
         newListItems:items,
     });
 
 });
 
-app.post("/", (req,res)=>{
-    item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+app.post("/",(req,res)=>{
+    const item = req.body.newItem;
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/Work")
+    } else {
+        items.push(item);
+        res.redirect("/")
+    };
 });
 
+app.get("/work", (req,res)=>{
+    res.render("list",{listTitle:"Work List", newListItems:workItems});
+});
+
+app.get("/about", (req,res)=>{
+    res.render("a")
+});
 
 app.listen(4000, ()=>{
     console.log(`Server is running now port 4000`);
